27cf8903391b0accd5dfcc90b7b4c5c92b1855c8
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-04 01:04:38 +0530

    gereating a controller and a api to registering user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,13 +2,22 @@ const User = require("../models/user.model");
 
 async function registerUser(req, res) {
     const { username,email, password, role="user" } = req.body;
-    const isUserAlreadyExist = await User.findOne(
+    const isUserAlreadyExist = await User.findOne({
+       $or:[{username},{email}]
+    });
+    if(isUserAlreadyExist){
+        return res.status(400).json({message:"User already exists"});
+    }
+    const user = await User.create({
         username,
         email,
         password,
         role
-    );
-    
+    });
+    const token = jwt.sign({
+        id:user._id,
+        role:user.role
+    },process.env.JWT_SECRET,{expiresIn:"1d"});
     
 }
 
