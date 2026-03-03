15f93555ebaf75b7a5b5acb76056da3639ddd8ad
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-04 01:37:31 +0530

    changes in login register api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,10 +8,13 @@ async function registerUser(req, res) {
     if(isUserAlreadyExist){
         return res.status(400).json({message:"User already exists"});
     }
+    const hash = await bcrypt.hash(password,10);
+    //here  10 is a salt value
+    //ye thoda na attacker ko delay karta hai 
     const user = await User.create({
         username,
         email,
-        password,
+        password:hash,
         role
     });
     const token = jwt.sign({
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/auth.controller");
 
-router.post("/register", (req, res) => {
-    res.send("Register");
-});
+router.post("/register", authController.registerUser);
 
 module.exports = router;
