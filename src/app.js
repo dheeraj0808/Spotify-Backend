5394ccb6e9342de9d6f65fea56577ed3fbc5d30a
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-10 17:23:38 +0530

    working on role based authentication system

--- src/app.js
+++ src/app.js
@@ -1,12 +1,39 @@
 const express = require("express");
-const userRoutes = require("./routes/auth.routes");
+
+// ROUTES
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+const artistRoutes = require("./routes/artist.routes");
+const adminRoutes = require("./routes/admin.routes");
 const musicRoutes = require("./routes/music.routes");
 
 const app = express();
 
+
+
+// Parse JSON body
 app.use(express.json());
 
+
+// Authentication routes
+app.use("/api/auth", authRoutes);
+
+// Normal user routes
 app.use("/api/users", userRoutes);
-app.use("/api/music", musicRoutes);
+
+// Artist routes
+app.use("/api/artists", artistRoutes);
+
+// Admin routes
+app.use("/api/admin", adminRoutes);
+
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Spotify Backend API running "
+  });
+});
+
 
 module.exports = app;
