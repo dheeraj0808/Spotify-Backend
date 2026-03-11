810e5caaa998234bcd27d606a942a21110506b0a
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-12 00:44:49 +0530

    creating a api in which artist role are verified and artist can upload music

--- src/model/music.model.js
+++ src/model/music.model.js
@@ -2,7 +2,6 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
 const Music = sequelize.define("Music", {
-
   uri: {
     type: DataTypes.STRING,
     allowNull: false
@@ -11,7 +10,6 @@ const Music = sequelize.define("Music", {
     type: DataTypes.STRING,
     allowNull: false
   },
-
   artist: {
     type: DataTypes.STRING,
     references: {
@@ -20,54 +18,8 @@ const Music = sequelize.define("Music", {
     },
     allowNull: false
   },
+  
 
-  album: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
-  genre: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
-  duration: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-
-  url: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
-  image: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-
-  lyrics: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-
-  releaseDate: {
-    type: DataTypes.DATE,
-    allowNull: false
-  },
-
-  createdAt: {
-    type: DataTypes.DATE,
-    allowNull: false
-  },
-
-  updatedAt: {
-    type: DataTypes.DATE,
-    allowNull: false
-  }
-
-}, {
-  timestamps: true
 });
 
 module.exports = Music;
