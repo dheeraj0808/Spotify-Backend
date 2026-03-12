8481c7b03d1546b05cfed97c59cc7755c7e05661
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-12 23:15:31 +0530

    changes for music routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,10 @@ const router = require("express").Router();
 const auth = require("../middleware/auth.middleware");
 const userController = require("../controller/user.controller");
 
-router.get("/profile",auth,userController.getProfile);
+router.get("/profile", auth, userController.getProfile);
 
-router.get("/users",auth,userController.getAllUsers);
+router.put("/profile", auth, userController.updateProfile);
+
+router.get("/users", auth, userController.getAllUsers);
 
 module.exports = router;
