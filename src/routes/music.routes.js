373a954a9b15cc258d4cb0a1083dc543c75b284b
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-13 23:52:31 +0530

    working on music routes to upload music functionality

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,1 +1,7 @@
-module.exports = require("express").Router();
+const express = require("express");
+const router = express.Router();
+const { createMusic } = require("../controller/music.controller");
+
+router.post("/create", createMusic);
+
+module.exports = router;
