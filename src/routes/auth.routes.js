16895c8be473ce4689724645d7d8eafa62282903
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-17 23:56:37 +0530

    chore: add routes for forget password route in auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,5 +5,5 @@ const authController = require("../controller/auth.controller");
 router.post("/register",authController.registerUser);
 router.post("/login",authController.loginUser);
 router.patch("/forgetPassword",authentication.forgetPassword);
-
+// routes for the forget password
 module.exports = router;
