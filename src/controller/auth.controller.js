6d17277534a23873d0c6c2f75dd69602ba4ddc60
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-17 13:18:11 +0530

    creating chagne passord api

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,7 +1,7 @@
 const User = require("../model/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const { secureHash, secureVerify, isStrong } = require('argon2-password');
 
 const registerUser = async (req, res) => {
     try {
@@ -13,6 +13,11 @@ const registerUser = async (req, res) => {
                 message: "All fields are required"
             });
         }
+        const strength = isStrong(password);
+        if (!strength.strong) {
+            return res.status(400).json({ errors: strength.errors });
+        }
+//   return
 
         const existingUser = await User.findOne({
             where: { email: email.toLowerCase() }
@@ -23,8 +28,9 @@ const registerUser = async (req, res) => {
                 message: "User already exists"
             });
         }
+        const hashedPassword = await secureHash(password);
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+        // const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = await User.create({
             name,
@@ -68,10 +74,13 @@ const loginUser = async (req, res) => {
                 message: "User not found"
             });
         }
+console.log(user.password , user.passwordHash)
+        const isMatch1 = await bcrypt.compare(password, user.password);
 
-        const isMatch = await bcrypt.compare(password, user.password);
-
-
+        const isMatch = await secureVerify(password, user.password);
+        if (!isMatch ) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
         if (!isMatch) {
             return res.status(401).json({
                 message: "Invalid password"
@@ -81,7 +90,7 @@ const loginUser = async (req, res) => {
         const token = jwt.sign(
             { id: user.id, role: user.role, },
             process.env.JWT_SECRET,
-            { expiresIn: "1m" },
+            { expiresIn: "1d" },
 
         );
 
