e0f959d54cdde73276016b3c2011d50650fe2fe9
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-30 17:14:06 +0530

    implement express-rate-limit to secure auth routes against brute-force attacks

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
@@ -11,6 +12,14 @@ const likeRoutes = require("./routes/like.routes");
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+
+
 app.use(cors({
     origin: ["http://localhost:5173", "http://localhost:5172"],
     credentials: true,
@@ -19,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", limiter, authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", musicRoutes);
 app.use("/api", playlistRoutes);
