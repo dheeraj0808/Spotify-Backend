bae5f22702e8232b75e5c509f81c562202d74e5c
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-04-03 19:46:02 +0530

    eorganize route structure and consolidate controller logic for music and user modules

--- src/routes/like.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const likeController = require("../controller/like.controller");
-const authMiddleware = require("../middleware/auth.middleware");
-
-router.post("/like", authMiddleware, likeController.likeMusic);
-router.post("/unlike", authMiddleware, likeController.unlikeMusic);
-router.get("/liked/:userId", authMiddleware, likeController.getLikedMusic); 
-
-module.exports = router;
